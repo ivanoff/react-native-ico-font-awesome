0fc0f496ec89404929cc73e99ba6e19ce6b94c60
Dimitry Ivanov <2@ivanoff.org.ua>
2023-09-09 22:45:05 +0300

    add types

--- typings.d.ts
+++ typings.d.ts
@@ -371,10 +371,35 @@ declare module 'react-native-ico-font-awesome' {
       'zoom-in' |
       'zoom-out';
 
+    type backgroundType = 'circle' | 'rect' | 'button';
+
+    type positionType = 'top' | 'bottom' | 'right' | 'left' |
+      'top_right' | 'top_left' | 'bottom_right' | 'bottom_left';
+
+    type backgroundTypeObj = {
+      type?: backgroundType;
+      color?: string;
+      radius?: number;
+      borderRadius?: number;
+    };
+
+    type badgeTypeObj = {
+      value?: number | string;
+      fontSize?: string;
+      position?: positionType;
+      color?: string;
+      backgroundColor?: string;
+      radius?: number;
+    };
+
     interface IconProps {
       name: iconNames;
-      color: string;
-      size: number;
+      width?: number;
+      height?: number;
+      color?: string;
+      stroke?: number;
+      background?: backgroundType | backgroundTypeObj;
+      badge?: number | string | badgeTypeObj;
     }
 
     const Icon: (props: IconProps) => ReactNode;
@@ -382,4 +407,3 @@ declare module 'react-native-ico-font-awesome' {
     export { iconNames };
     export default Icon;
 }
-
