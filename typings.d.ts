6a60ed48c260468caac39a35a84f576e7907eb80
Dimitry Ivanov <2@ivanoff.org.ua>
2023-10-01 18:36:35 +0300

    fix types

--- typings.d.ts
+++ typings.d.ts
@@ -1,5 +1,6 @@
 declare module 'react-native-ico-font-awesome' {
     import { ReactNode } from 'react';
+    import { SvgProps, AdditionalProps } from 'react-native-svg';
 
     type iconNames = 'add-square-button' |
       'adjust-contrast' |
@@ -402,7 +403,7 @@ declare module 'react-native-ico-font-awesome' {
       badge?: number | string | badgeTypeObj;
     }
 
-    const Icon: (props: IconProps) => ReactNode;
+    const Icon: (props: IconProps & SvgProps & AdditionalProps) => ReactNode;
 
     export { iconNames };
     export default Icon;
